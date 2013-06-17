4bb54b319ccee10aaea5717577ecd8471b56e370
Thomas Maroschik <tmaroschik@dfau.de>
2013-06-17 11:22:43 +0200

    More debug info

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email	"kyle.bader@dreamhost.com"
 license				"Apache 2.0"
 description			"Installs/Configures the Ceph distributed filesystem"
 long_description	IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version				"0.1.2"
+version				"0.1.3"
 depends				"apache2", ">= 1.1.12"
 depends 			"apt"
--- recipes/osd.rb
+++ recipes/osd.rb
@@ -105,6 +105,7 @@ else
     #  - $cluster should always be ceph
     #  - The --dmcrypt option will be available starting w/ Cuttlefish
     unless node["ceph"]["osd_devices"].nil?
+p node["ceph"]["osd_devices"]
       node["ceph"]["osd_devices"].each_with_index do |osd_device,index|
 p osd_device
         if not osd_device["status"].nil?
